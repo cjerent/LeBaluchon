5782c310f700c603da47f739704112285a7e5749
cjerent <charlenejerent@gmail.com>
2023-06-06 20:34:43 +0200

    total design changes (logo, font etc)

--- Le Baluchon/Controller/ExchangeRateViewController.swift
+++ Le Baluchon/Controller/ExchangeRateViewController.swift
@@ -8,6 +8,62 @@
 import UIKit
 
 class ExchangeRateViewController: UIViewController {
+    
+    @IBOutlet weak var numberTextField: UITextField!
+    @IBOutlet weak var currencyPickerView: UIPickerView!
+    @IBOutlet weak var convertButton: UIButton!
+    
+    override func viewDidLoad() {
+      super.viewDidLoad()
+
+      let myColor = UIColor.customViolet
+        numberTextField.layer.borderColor = myColor.cgColor
+        numberTextField.layer.cornerRadius = 5.0
+        numberTextField.layer.borderWidth = 1.0
+        convertButton.layer.cornerRadius = 5.0
+
+   }
+    
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        numberTextField.resignFirstResponder()
+    }
+}
 
 
+
+extension ExchangeRateViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        currencies.count
+    }
+    
+    
+}
+
+
+
+extension UIColor {
+    static let customViolet = UIColor(red: 88/255.0, green: 112/255.0, blue: 247/255.0, alpha: 1)
+}
+
+extension ExchangeRateViewController: UIPickerViewDelegate {
+//    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+//        return currencies[row]
+//    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+    
+        var pickerLabel: UILabel? = (view as? UILabel)
+        if pickerLabel == nil {
+            pickerLabel = UILabel()
+            pickerLabel?.font = UIFont(name: "Cochin", size: 30)
+            pickerLabel?.textAlignment = .center
+        }
+        pickerLabel?.text = currencies[row]
+        pickerLabel?.textColor = UIColor.customViolet
+        return pickerLabel!
+    }
 }
--- Le Baluchon/Controller/TranslationViewController.swift
+++ Le Baluchon/Controller/TranslationViewController.swift
@@ -8,5 +8,19 @@
 import UIKit
 
 class TranslationViewController: UIViewController {
+    @IBOutlet weak var sentenceToTranslateTextView: UITextView!
+    
+    override func viewDidLoad() {
+      super.viewDidLoad()
 
+      let myColor = UIColor.customViolet
+        sentenceToTranslateTextView.layer.borderColor = myColor.cgColor
+        sentenceToTranslateTextView.layer.cornerRadius = 5.0
+        sentenceToTranslateTextView.layer.borderWidth = 1.0
+        
+
+   }
+    @IBAction func dismiss(_ sender: UITapGestureRecognizer) {
+        sentenceToTranslateTextView.resignFirstResponder()
+    }
 }
