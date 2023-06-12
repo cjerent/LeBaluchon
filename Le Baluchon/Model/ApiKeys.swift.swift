651e30011481ec1bdf2b109515aa4d30d9ee3222
cjerent <charlenejerent@gmail.com>
2023-06-12 20:10:32 +0200

    add .gitignore file

--- Le Baluchon/Model/ApiKeys.swift.swift
+++ Le Baluchon/Model/ApiKeys.swift.swift
@@ -12,5 +12,5 @@ func valueForAPIKey(named keyname:String) -> String {
     let filePath = Bundle.main.path(forResource: "ApiKeys", ofType: "plist")
 let plist = NSDictionary(contentsOfFile:filePath!)
 let value = plist?.object(forKey: keyname) as! String
-return value
+return value 
 }
